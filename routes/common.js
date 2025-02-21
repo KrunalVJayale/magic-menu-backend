17442a5a130b6d8d46c2f32437fca1cc89af3a13
KrunalVJayale <krunaljayale5786257@gmail.com>
2025-02-22 00:14:40 +0530

    Delete routes directory

--- routes/common.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const wrapAsync = require("../utils/wrapAsync");
-const { category } = require("../controllers/common");
-
-
-router.get("/category", wrapAsync(category));
-
-module.exports = router;
