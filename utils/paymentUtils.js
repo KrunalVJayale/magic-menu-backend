5a8b65ceb42da14e722e92cb7ad665ce019f5e0e
KrunalVJayale <krunaljayale5786257@gmail.com>
2025-05-24 23:37:28 +0530

    Delete utils directory

--- utils/paymentUtils.js
+++
@@ -1,8 +0,0 @@
-module.exports.generateTransactionID = () => {
-  return `T${Date.now()}${Math.floor(Math.random() * 1000000)}`;
-};
-
-
-module.exports.generateTicket = () =>{
-  return (Math.floor(100000 + Math.random() * 900000));
-}
