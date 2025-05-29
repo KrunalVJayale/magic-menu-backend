75f49ead7774cb234140fc8eb6d97b532509882a
Krunal Jayale <krunaljayale@gmail.com>
2025-05-29 16:42:29 +0530

    Updated

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
-// const serviceAccount = require("/etc/secrets/magicmenu-riderapp-firebase-adminsdk-fbsvc-850c2bcadf.json"); // Download this from Firebase Console > Project Settings > Service accounts
+const serviceAccount = require("/etc/secrets/magicmenu-riderapp-firebase-adminsdk-fbsvc-850c2bcadf.json"); // Download this from Firebase Console > Project Settings > Service accounts
 
-const serviceAccount = require("../assets/magicmenu-riderapp-firebase-adminsdk-fbsvc-850c2bcadf.json");
+// const serviceAccount = require("../assets/magicmenu-riderapp-firebase-adminsdk-fbsvc-850c2bcadf.json");
 
 if (!admin.apps.length) {
   admin.initializeApp({
